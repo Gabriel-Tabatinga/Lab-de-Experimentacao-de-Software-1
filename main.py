ef59f1d48ad643534731d2a1af5fd4b2f1f033bf
Gabriel Pimentel Tabatinga <80406092+Gabriel-Tabatinga@users.noreply.github.com>
2025-08-22 21:08:06 -0300

    url funcionando
    url está funcionando e esta chegando certinho os items no console

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import requests
 
-token = ""
+with open('token.txt', 'r') as file:
+    token = file.read().strip()
 
 headers = {"Authorization": f"token {token}"}
 url = f"https://api.github.com/search/repositories?q=stars:%3E0&sort=stars&order=desc&per_page=100&page=1"
 response = requests.get(url, headers=headers)
 
-if responses.status_code == 200:
-    return response.json()["items"]
-else 
+if response.status_code == 200:
+    data = response.json()["items"]
+else: 
     raise Exception
+
+print(data)
